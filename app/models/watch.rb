cecea70ef6e794ec3b38142af313814ba2ec9fd0
Cannikin <cannikinn@gmail.com>
2009-11-03 15:38:42 -0800

    Finding average based on samples of intervals (10 samples of 10 minutes each)

--- app/models/watch.rb
+++ app/models/watch.rb
@@ -8,6 +8,23 @@ class Watch < ActiveRecord::Base
   
   before_save :set_status
   
+  DEFAULT_INTERVAL = 10.minutes
+  DEFAULT_COUNT = 10
+  
+  # Computes the standard deviation for the last response time of this watch compared to a certain number of checks in the past.
+  # By default we look at the average of the last 10 ten minute spans
+  def from_average(interval=DEFAULT_INTERVAL, count=DEFAULT_COUNT)
+    # puts interval
+    averages = []
+    1.upto(count) do |i|
+      averages << Response.average(:time, :conditions => ['watch_id = ? and time != 0 and created_at < ? and created_at > ?', self.id, (Time.zone.now-(interval*i)).to_s(:db), (Time.zone.now - (interval*i+interval)).to_s(:db)]).to_i
+    end
+    averages.reject! { |x| x == 0 }   # remove 0 values
+    average = averages.inject(nil) { |sum,x| sum ? sum + x : x } / averages.length
+    logger.debug("  *** averages: #{averages.inspect}, average: #{average}, last_response_time: #{self.last_response_time}")
+    return average.to_f / self.last_response_time.to_f * 100.0
+  end
+  
   private
     
     def set_status
@@ -15,4 +32,5 @@ class Watch < ActiveRecord::Base
         self.status_id = Status::DISABLED
       end
     end
+    
 end
